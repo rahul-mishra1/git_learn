70cd332cad716c308129a2a809be91cd6702bbe6
Rahul <rahul.mishra@anchanto.com>
2021-01-07 15:10:22 +0530

    Introduce dog function

--- animal.rb
+++ animal.rb
@@ -1,7 +1,15 @@
 class Animal
 
-  def call
-    puts "Hello I am Animal"
+  def dog
+    puts "Hello I am dog"
+  end
+
+  def call(animal_name = nil)
+    if animal_name == 'dog'
+      dog
+    else
+      puts "Hello I am Animal"
+    end
   end
 end
-Animal.new.call
+Animal.new.call('dog')
